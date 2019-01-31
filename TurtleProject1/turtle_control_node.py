e24ff9481655294e3b8814d848a588bf4d876aeb
Nathan Toombs <natetoombs@gmail.com>
2019-01-31 09:26:46 -0700

    Added control node

---
+++ TurtleProject1/turtle_control_node.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+#cmd = 'nogo'
+
+#def callback(msg):
+#    x = msg
+#    rospy.loginfo(x)
+#    if x != 'Teleoperation':
+##        cmd = input("Enter Command: ")
+#    else:
+#        cmd = 'nogo'
+
+
+def control():
+    rospy.init_node('turtle_control', anonymous = True)
+    control_publisher = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+    control_msg = Twist()
+
+    control_msg.linear.x = 0
+    control_msg.linear.y = 0
+    control_msg.linear.z = 0
+    control_msg.angular.z = 0
+    control_msg.angular.x = 0
+    control_msg.angular.y = 0
+
+
+    while not rospy.is_shutdown():
+        cmd = raw_input("type 'straight' or 'turn':  ")
+
+        if cmd == 'straight':
+            speed = input("Enter your speed: ")
+            distance = input("Enter your distance: ")
+            control_msg.linear.x = speed
+            #speed*time=distance
+            current_distance = 0
+            t0 = rospy.Time.now().to_sec()#current time, float
+            while (current_distance < distance):
+                control_publisher.publish(control_msg)
+                t1 = rospy.Time.now().to_sec()
+                current_distance = speed*(t1-t0)#v*dt = d
+
+            control_msg.linear.x = 0
+            control_publisher.publish(control_msg)
+
+        if cmd == 'turn':
+            speed = input("Enter your speed: ")#rad/sec
+            angle = 3.14159265/180*input("Enter your turn angle (deg): ")#deg
+            control_msg.angular.z = speed
+            #angle = speed*time
+            current_angle = 0#rad
+            t0 = rospy.Time.now().to_sec()#current time, float
+            while (current_angle < angle):
+                control_publisher.publish(control_msg)
+                t1 = rospy.Time.now().to_sec()
+                current_angle = speed*(t1-t0)#v*dt = d
+
+            control_msg.angular.z = 0
+            control_publisher.publish(control_msg)
+
+        cmd = None
+
+    #rospy.Subscriber("/cmd_vel_mux/active", String, callback)
+    #rospy.spin()
+
+if __name__ == '__main__':
+        try:
+            control()
+        except rospy.ROSInterruptException: pass
