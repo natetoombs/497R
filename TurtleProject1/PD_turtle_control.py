7f8afae88b7a7adda99eb20852e275b686c5faee
Nathan Toombs <natetoombs@gmail.com>
2019-02-01 17:34:57 -0700

    Trying again to commit

---
+++ TurtleProject1/PD_turtle_control.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+def control():
+	rospy.init_node('PD_turtle_control', anonymous = True)
+	control_publisher = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+	control_msg = Twist()
+
+	control_msg.linear.x = 0
+	control_msg.linear.y = 0
+	control_msg.linear.z = 0
+	control_msg.angular.x = 0
+	control_msg.angular.y = 0
+	control_msg.angular.z = 0
+
+	control_subscriber = rospy.Subscriber()
+
+	while not rospy.is_shutdown():
+		poscx = 
+		poscy = 
+
+
+		#print("Your current position is: ")
+		#print(poscx)
+		#print(poscy)
+		posx = input("Give your desired 'x' coordinate: ")
+		posy = input("Give your desired 'y' coordinate: ")
+
